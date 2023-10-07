1df91ef474cd62fb46c4d56915e07cb453e6d332
Caio Santanna <caiorsantanna@gmail.com>
2023-10-06 22:11:35 -0300

    feat: 🎸 estrutura inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home-routing.module').then((module) => module.HomeRoutingModule),
+    title: 'Roteirizador - Field Control'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
